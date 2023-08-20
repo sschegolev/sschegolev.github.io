194a35c7e8b5c46c15115f3d6b394e1f33770e26
Siarhei Shchohaleu <sergey.schegolev@pandadoc.com>
2023-08-20 17:21:29 +0200

    Updates

--- src/components/Main.js
+++
@@ -1,9 +0,0 @@
-import {usePlayers} from "../hooks/usePlayers";
-import {Table} from "./Table";
-
-
-export const Main = () => {
-  const {players} = usePlayers();
-
-  return players.length ? <Table players={players} /> : null;
-}
